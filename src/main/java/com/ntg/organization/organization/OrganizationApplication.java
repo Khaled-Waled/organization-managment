176a569d11a37682227162ca4a821a38be878288
Khaled-Waled <khaledw.hamed001@gmail.com>
2022-08-09 08:26:32 +0200

    Added Swagger2 dependency.

--- src/main/java/com/ntg/organization/organization/OrganizationApplication.java
+++ src/main/java/com/ntg/organization/organization/OrganizationApplication.java
@@ -8,7 +8,8 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OrganizationApplication {
-
+//for api documentation open:
+//http://localhost:8090/swagger-ui.html
 	public static void main(String[] args) {
 		SpringApplication.run(OrganizationApplication.class, args);
 	}
